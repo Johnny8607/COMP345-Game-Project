8563f9a4f825889cac1bd70b12ff9c1005be2c54
Johnny8607 <dangjohnny0543@gmail.com>
2025-11-11 13:21:37 -0500

    fix merge conflicts from Part2 - v2

--- Assignment2/MainDriver.cpp
+++ Assignment2/MainDriver.cpp
@@ -9,7 +9,8 @@ void testCommandProcessor();
 void testStartupPhase();
 void testMainGameLoop();
 
-void pressEnterToContinue() {
+void pressEnterToContinue()
+{
     std::cout << "Press Enter to continue...";
     std::cin.ignore(); // Ignore any leftover characters
     std::cin.get();    // Wait for Enter key
@@ -17,10 +18,12 @@ void pressEnterToContinue() {
 }
 
 int main()
-{     
-    std::cout << "\n******************************\n" << std::endl;
+{
+    std::cout << "\n******************************\n"
+              << std::endl;
     std::cout << "=== COMP 345 Assignment 1 ===" << std::endl;
-    std::cout << "\n******************************\n" << std::endl;
+    std::cout << "\n******************************\n"
+              << std::endl;
 
     std::cout << "Testing Part 1: Map" << std::endl;
     std::cout << "==============================\n"
@@ -50,15 +53,19 @@ int main()
     std::cout << "==============================\n"
               << std::endl;
     testGameStates();
-    std::cout << "\n******************************\n" << std::endl;
+
+    std::cout << "\n******************************\n"
+              << std::endl;
     std::cout << "=== COMP 345 Assignment 2 ===" << std::endl;
-    std::cout << "\n******************************\n" << std::endl;
-    
+    std::cout << "\n******************************\n"
+              << std::endl;
+
     std::cout << "Testing Assignment 2 Part 1: Command Processor" << std::endl;
-    std::cout << "==============================\n" << std::endl;
+    std::cout << "==============================\n"
+              << std::endl;
     testCommandProcessor();
     pressEnterToContinue();
-
+    
     std::cout << "Testing Assignment 2 Part 2: Game Startup Phase" << std::endl;
     std::cout << "==============================\n" << std::endl;
     testStartupPhase();
